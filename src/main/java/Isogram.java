fb05347bf120e8817f0c4de355a374e6d7425043
Yonatan Tadesse <enyonathan@gmail.com>
2023-07-13 17:53:43 +0000

    isogram solved using HashSet

--- src/main/java/Isogram.java
+++ src/main/java/Isogram.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 
 public class Isogram {
     /**
@@ -13,6 +14,11 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        HashSet<Character> setString = new HashSet<>();
+        for (int i=0; i<str.length(); i++){
+            if (!setString.add(str.charAt(i)))
+                return false;
+        }
+        return true;
     }
 }
